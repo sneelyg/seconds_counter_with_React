6891e0ab926174224ec1316af73adade4fcea563
sneelyg <santiagoneely@gmail.com>
2022-08-06 01:37:56 +0000

    buttons progressing. a lot left

--- src/js/component/botones.js
+++ src/js/component/botones.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-const Botones = (props) => {
-  
-
+const Botones = () => {
   return (
     <div className="row justify-content-center mt-5">
-     
       <div className="row justify-content-center mt-5">
-      <div className="col-2 mx-2" id="stop_btn">Stop</div>
-      <div className="col-2 mx-2" id="start_btn">Start</div>
-      <div className="col-2 mx-2" id="stop_btn">Reset</div> 
+        <div className="col-2 mx-2" id="stop_btn">
+          Stop
+        </div>
+        <div className="col-2 mx-2" id="start_btn">
+          Start
+        </div>
+        <div className="col-2 mx-2" id="stop_btn" onClick={()=>seconds=0}>
+          Reset
+        </div>
+        <div className="col-2 mx-2" id="stop_btn">
+          <button onClick={()=>seconds=0}>ClickMe</button>
+        </div>
       </div>
     </div>
   );
